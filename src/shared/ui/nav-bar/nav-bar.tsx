63ea6a6d1628deffdbffa0a5d5fe6cbded677e1f
Raphael Mansueto <raphaelmansueto@gmail.com>
2023-04-25 01:06:26 +0800

    fixed sticky

--- src/shared/ui/nav-bar/nav-bar.tsx
+++ src/shared/ui/nav-bar/nav-bar.tsx
@@ -49,6 +49,7 @@ const NavBar = (props: Props) => {
         alignItems: "center",
         boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25);",
         position: "sticky",
+        top: 0,
       }}
       px={48}
       w="100%"
